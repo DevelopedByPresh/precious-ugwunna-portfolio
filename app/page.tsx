17b4eefa002de772c5ff3b3b815ca807f022cb43
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-05-29 21:13:49 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="font-clash text-black bg-[#caf0f8] m-3 text-4xl">
-      <Header />
-      <section id="hero">
+    <div className="font-clash antialiased  text-black snap-mandatory snap-y z-0 m-3 text-4xl">
+      
+      <section id="hero" className="snap-start">
         <Hero />
+      </section>
+      <section id="about" className="snap-center">
+        <About/>
       </section>
     </div>
   );
