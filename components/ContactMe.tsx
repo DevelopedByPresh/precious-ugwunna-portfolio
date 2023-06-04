1f43ff6395bdb2e5b6c379044039a2472d474db1
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-04 19:22:18 +0100

    contact commit 2nd

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+"use client"
 import React from "react";
 import Button from "./Button";
 import { SocialIcon } from "react-social-icons";
@@ -6,6 +7,9 @@ import { SocialIcon } from "react-social-icons";
 type Props = {};
 
 const ContactMe = (props: Props) => {
+  const handleClick = () => {
+    window.location.href = "mailto:ayodejiatanda1@gmail.com";
+  };
   return (
     <div
       className="relative flex text-center flex-col md:text-left md:flex-row max-w-7xl
@@ -16,22 +20,30 @@ const ContactMe = (props: Props) => {
         Get In Touch
       </h3>
       <div className="flex flex-col items-center justify-center text-center max-w-3xl tracking-widest 
-       font-normal xl:font-semibold m">
-         <p className="text-sm xl:mb-16">
+       font-normal xl:text m">
+         <p className="text-sm xl:mb-16 xl:text-xl xl:font-semibold">
          Ready to take your web projects to the next <span className="underline decoration-black decoration-2">level</span>? <br/> Let's collaborate and create exceptional 
          user experiences together.
          <br/> Contact me now and let's discuss 
          how I can contribute to your frontend development needs. Reach out and let's build something amazing!
          </p>
             <Button title="Say Hello" />
-            <div className="absolute bottom-10 space-x-4">
+            <div className="absolute bottom-10 space-x-4 xl:space-y-5 xl:bottom-[11rem] xl:left-5 xl:space-x-0 xl:flex xl:flex-col xl:z-20 xl:fixed xl:items-center xl:justify-center xl:text-center">
+            <SocialIcon
+          network="email"
+          bgColor="transparent"
+          fgColor="#464646"
+          onClick={handleClick}
+          target="_blank"
+          className="h-2 w-2 filter grayscale  hover:filter-none hover:animate-pulse transition duration-200 ease-in-out"
+        />
       <SocialIcon
           network="github"
           bgColor="transparent"
           fgColor="#464646"
           url="https://github.com/deji-ice"
           target="_blank"
-          className="h-2 w-2  hover:text-black hover:animate-pulse transition duration-200 ease-in-out rounded-full border-spacing-10 "
+          className="h-2 w-2 filter grayscale  hover:filter-none hover:animate-pulse transition duration-200 ease-in-out"
         />
       <SocialIcon
           network="twitter"
@@ -58,7 +70,7 @@ const ContactMe = (props: Props) => {
           className="h-2 w-2 filter grayscale  hover:filter-none hover:animate-pulse transition duration-200 ease-in-out"
         />
       </div>
-      <p className="text-xs absolute bottom-5 tracking-wide font-normal">Created by twitter user @dejixice @2023</p>
+      <p className="text-xs absolute bottom-5 tracking-wide font-normal">Created by twitter user <span className="font-semibold"> @dejixice</span> @2023</p>
       </div>
      
     </div>
