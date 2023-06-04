4c0d79827758be8733a470aac1fe9ab78ac671e3
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-04 19:22:49 +0100

    bg issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import "./globals.css";
 import { Inter } from "@next/font/google";
+import bg from "../Assets/stacked-peaks-haikei (3).png"
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
@@ -14,8 +15,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="font-clash antialiased text-black snap-mandatory snap-y z-0 m-3 text-4xl
-    overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-white scrollbar-thumb-black">
+    <div 
+   
+    className=" font-clash antialiased text-black snap-mandatory snap-y z-0 m-3 text-4xl
+    overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-white scrollbar-thumb-black bg-no-repeat bg-[pink] bg-center bg-fixed"
+    
+    
+    style={{backgroundImage:`url(${bg})`, }}> 
       
       <section id="hero" className="snap-start">
         <Hero />
@@ -34,6 +40,9 @@ export default function Home() {
       </section>
       <section id="contact" className="snap-start">
         <ContactMe/>
+      </section>
+      <section>
+
       </section>
     </div>
   );
