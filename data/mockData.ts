017c42cd56962c7a1f5fe4cc2ce49dfb59c85c32
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-05 18:18:46 +0100

    data mock

---
+++ data/mockData.ts
@@ -0,0 +1,38 @@
+interface ImageData {
+    alt: string;
+    src: string;
+    progress: string
+}
+
+export const images: ImageData[] = [
+    {
+        alt: "React",
+        src: "https://asset.brandfetch.io/idbkMiejO4/idee5k_Xjl.svg",
+        progress: "85%"
+    },
+    {
+        alt: "typescript",
+        src: "https://asset.brandfetch.io/idKX_Hb7va/id1t_VNpUn.svg",
+        progress: "85%"
+    },
+    {
+        alt: "javascript",
+        src: "https://asset.brandfetch.io/idq5ck33b2/id8Fb4RR16.png",
+        progress: "85%"
+    },
+    {
+        alt: "tailwind-css",
+        src: "https://asset.brandfetch.io/idKJ12s-EY/idI9erPtdw.jpeg",
+        progress: "85%"
+    },
+    {
+        alt: "firebase",
+        src: "https://asset.brandfetch.io/ideatLZc2I/idlwOLs00g.jpeg",
+        progress: "85%"
+    },
+    {
+        alt: "framer",
+        src: "https://asset.brandfetch.io/idCeIE9B96/id672ybz9X.svg",
+        progress: "85%"
+    },
+];
