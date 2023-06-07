e76ae66ef414129c7aa89b71a131291d9b1b33eb
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-07 08:01:39 +0100

    experience cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,55 +1,78 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client"
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from "react";
+import { experiences } from "@/data/mockData";
 
-// Import Swiper styles
-import 'swiper/css';
-import logo from "Assets/Untitled-2-Recovered.png";
-
-import React from "react";
-import { motion } from "framer-motion";
-import Image from "next/image";
 
 type Props = {};
 
 const Cards = ({}: Props) => {
-  return (
-    <article className="flex flex-col rounded-lg items-center space-y-4  border-x-neutral-700 border-2  flex-shrink-0 w-[270px] md:w-[600px] 
-    xl:w-[650px] snap-center no-scrollbar  p-2 focus:opacity-100 hover:opacity-100 cursor-pointer transition-opacity
-    duration-200 overflow-hidden overflow-y-scroll">
-      {/* <motion.img
-        initial={{
-          y: -100,
-          opacity: 0,
-        }}
-        transition={{ duration: 1.5 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        className="w-14 h-14 xl:w-[70px] xl:h-[70px] object-cover object-center rounded-full"
-        alt="logo"
-        src={logo.src}
-      /> 
-      */}
-      <div className="px-0 md:px-10">
-        <h4 className="text-xl lg:text-3xl font-light">Frontend Developer</h4>
-        <p className="font-bold  text-xl">Sabiroad</p>
-        <div className="flex space-x-1 my-1">
-            <img className="h-7 w-7 rounded-full" alt="img" src={logo.src}/>
-            <img className="h-7 w-7 rounded-full" alt="img" src={logo.src}/>
-            <img className="h-7 w-7 rounded-full" alt="img" src={logo.src}/>
-            <img className="h-7 w-7 rounded-full" alt="img" src={logo.src}/>
 
-        </div>
-        <p className="py-5 text-sm font-semibold xl:text-xs">04/2022 - 04/2023</p>
-        <ul className="space-y-4 w-52 md:w-full ml-5 list-disc text-sm">
-            <li>Lorem ipsum dolor sit amet, consectetur
-             adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-            <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </li>
-            <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </li>
-        </ul>
-      </div>
-    </article>
+  const [selectedCompany, setSelectedCompany] = useState<string | null>(
+    experiences.length > 0 ? experiences[0].companyName : null
+  );
+
+  const handleCompanyClick = (companyName: string) => {
+    setSelectedCompany(companyName);
+  };
+  return (
+    <article className="flex flex-col md:m-5 md:p-10 md:items-center snap-x snap-mandatory absolute top-[12rem] md:top-[9rem] lg:top-[5rem] xl:top-[6rem] w-full">
+    <ul className="flex  whitespace-nowrap  overflow-x-scroll mx-2 mb-5">
+      {experiences.map((experience) => (
+        <li
+        className={`inline-block px-4 py-2 text-sm  md:text-xl text-center items-center justify-center hover:bg-gray-200 transition duration-500 ease-in-out ${
+          selectedCompany === experience.companyName ? "border-b-2 border-[black] bg-gray-200" : ""
+        }`}
+          key={experience.companyName}
+          onClick={() => handleCompanyClick(experience.companyName)}
+        >
+          {experience.companyName}
+        </li>
+      ))}
+    </ul>
+    <div>
+      {selectedCompany && (
+        <>
+          <h3 className="text-xl md:text-2xl m-2 font-semibold">
+            {
+              experiences.find((exp) => exp.companyName === selectedCompany)
+                ?.occupation
+            }{"  "}
+            <span >@{"  "}{selectedCompany}</span>
+          </h3>
+
+          <p className="text-xs m-2">
+            {
+              experiences.find((exp) => exp.companyName === selectedCompany)
+                ?.dateStart
+            }{" "}
+            -{" "}
+            {
+              experiences.find((exp) => exp.companyName === selectedCompany)
+                ?.dateEnd
+            }
+          </p>
+
+          <ul className="text-sm md:text-xl m-2 list-disc space-y-2 pl-5">
+            {experiences
+              .find((exp) => exp.companyName === selectedCompany)
+              ?.responsibilities.map((responsibility, index) => (
+                <li key={index}>{responsibility}</li>
+              ))}
+            {/* <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat risus id massa pulvinar
+ condimentum. Donec eu tincidunt mauris. Mauris efficitur ex vel ex sollicitudin, nec euismod lorem 
+ imperdiet.</li>
+ <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat risus id massa pulvinar
+ condimentum. Donec eu tincidunt mauris. Mauris efficitur ex vel ex sollicitudin, nec euismod lorem 
+ imperdiet.</li>
+ <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat risus id massa pulvinar
+ condimentum. Donec eu tincidunt mauris. Mauris efficitur ex vel ex sollicitudin, nec euismod lorem 
+ imperdiet.</li> */}
+          </ul>
+        </>
+      )}
+    </div>
+  </article>
   );
 };
 
