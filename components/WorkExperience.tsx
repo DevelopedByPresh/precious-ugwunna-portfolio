b767822a0f9b4bfc846fd1764a424aa2f358656e
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-07 08:01:23 +0100

    experience

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,20 +1,29 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from "react";
+"use client"
+import { useState } from "react";
 import Cards from "./Cards";
+import { experiences } from "@/data/mockData";
 type Props = {};
 
 const WorkExperience = ({}: Props) => {
+  const [selectedCompany, setSelectedCompany] = useState<string | null>(
+    experiences.length > 0 ? experiences[0].companyName : null
+  );
+
+  const handleCompanyClick = (companyName: string) => {
+    setSelectedCompany(companyName);
+  };
+
   return (
-    <div 
+    <div
       className="h-screen flex relative overflow-hidden z-0 flex-col md:flex-row text-left px-10 lg:my-[10rem]
    max-w-full items-center pt-20 mx-auto justify-evenly no-scrollbar "
     >
       <h3 className="text-gray-500 absolute xl:text-3xl  xl:top-16 top-24 lg:top-0 text-3xl mb-5  md:text-5xl md:mb-[6rem] font-semibold uppercase tracking-[10px]">
         Experience
       </h3>
-      <div className="w-full flex space-x-5 m-5 overflow-x-scroll p-10 snap-x snap-mandatory ">
-        <Cards />
-      </div>
+
+      <Cards/>
     </div>
   );
 };
--- data/mockData.ts
+++ data/mockData.ts
@@ -4,6 +4,59 @@ interface ImageData {
     progress: string
 }
 
+type JobExperience = {
+    occupation: string;
+    companyName: string;
+    dateStart: string;
+    dateEnd: string;
+    responsibilities: string[];
+};
+
+type Project = {
+    id: number
+    projectName: string;
+    projectImage: string;
+    projectDescription: string;
+    demoLink: string;
+    githubLink: string;
+  };
+  
+  export const projects: Project[] = [
+    {
+        id: 1,
+      projectName: "ChatGPT Clone",
+      projectImage: "image1.jpg",
+      projectDescription: "Missing Tailwind CSS: Make sure you have properly included the Tailwind CSS library in your project. Check if you have imported it correctly in your main CSS file or added the CDN link in your HTML file. Without Tailwind CSS, the list styles may not be applied.",
+      demoLink: "https://www.example.com/project1-demo",
+      githubLink: "https://github.com/user/project1",
+    },
+    {
+        id: 2,
+        projectName: " Clone",
+        projectImage: "image1.jpg",
+        projectDescription: "Missing Tailwind CSS: Make sure you have properly included the Tailwind CSS library in your project. Check if you have imported it correctly in your main CSS file or added the CDN link in your HTML file. Without Tailwind CSS, the list styles may not be applied.",
+        demoLink: "https://www.example.com/project1-demo",
+        githubLink: "https://github.com/user/project1",
+      },
+    {
+        id: 3,
+      projectName: "Blog",
+      projectImage: "image2.jpg",
+      projectDescription: "Missing Tailwind CSS: Make sure you have properly included the Tailwind CSS library in your project. Check if you have imported it correctly in your main CSS file or added the CDN link in your HTML file. Without Tailwind CSS, the list styles may not be applied.",
+      demoLink: "https://www.example.com/project2-demo",
+      githubLink: "https://github.com/user/project2",
+    },
+    {
+        id: 4,
+      projectName: "Aricash",
+      projectImage: "image3.jpg",
+      projectDescription: "Missing Tailwind CSS: Make sure you have properly included the Tailwind CSS library in your project. Check if you have imported it correctly in your main CSS file or added the CDN link in your HTML file. Without Tailwind CSS, the list styles may not be applied.",
+      demoLink: "https://www.example.com/project3-demo",
+      githubLink: "https://github.com/user/project3",
+    },
+  ];
+  
+
 export const images: ImageData[] = [
     {
         alt: "React",
@@ -34,5 +87,31 @@ export const images: ImageData[] = [
         alt: "framer",
         src: "https://asset.brandfetch.io/idCeIE9B96/id672ybz9X.svg",
         progress: "85%"
+    },
+];
+
+
+export const experiences: JobExperience[] = [
+    {
+        occupation: "Frontend Developer",
+        companyName: "Sabrioad",
+        dateStart: "2022-01-01",
+        dateEnd: "2023-01-01",
+        responsibilities: [
+            "Developed user interfaces using React.js",
+            "Implemented responsive designs with CSS and media queries",
+            "Collaborated with the design team to bring concepts to life",
+        ],
+    },
+    {
+        occupation: "software Intern",
+        companyName: "First Bank",
+        dateStart: "2020-06-01",
+        dateEnd: "2020-12-31",
+        responsibilities: [
+            "Created wireframes and prototypes for web applications",
+            "Designed visually appealing user interfaces with Adobe XD",
+            "Worked closely with developers to ensure design implementation",
+        ],
     },
 ];
