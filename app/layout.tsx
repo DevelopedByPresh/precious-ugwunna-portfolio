470444a8682616edea5afa31d59ddccbbd8a4b82
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-09 22:11:52 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,26 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import localFont from '@next/font/local'
+
+const programme = localFont({
+  src: [
+    {
+      path: '../public/fonts/Programme-Regular.woff2',
+      weight:"400"
+    },
+  ],
+  variable: '--font-programme'
+})
+const montecatiniPro = localFont({
+  src: [
+    {
+      path: '../public/fonts/MontecatiniPro-StrettoUltra.woff2',
+      weight:"700"
+    },
+  ],
+  variable: '--font-montecatiniPro'
+})
 
 export default function RootLayout({
   children,
@@ -8,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${programme.variable} ${montecatiniPro.variable}  font-sans`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
