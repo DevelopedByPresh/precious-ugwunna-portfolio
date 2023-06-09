9615d1468b7d672325dcf24a44e1b143f3b6af13
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-09 22:11:40 +0100

    button ui

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BgCircles from "./BgCircles";
@@ -8,32 +9,31 @@ type Props = {};
 
 const Hero = ({}: Props) => {
   const [text, count] = useTypewriter({
-    words: ["Fullstack web dev.", "Frontend dev.", "Graphic Designer"],
+    words: ["Fullstack Web Developer.", "Frontend Developer.", "Graphic Designer."],
     loop: true,
     delaySpeed: 2000,
   });
   return (
     <motion.div
       className="flex flex-col m-5 xl:ml-[10rem] lg:pb-[18rem] items-start mt-[12rem] md:mt-[15rem]
-    lg:mt-[20rem] xl:mt-[8rem] md:max-w-2xl md:p-20 lg:max-w-[1500px] lg:m-0 xl:pt-0 h-screen overflow-hidden"
+    lg:mt-[20rem] xl:mt-[5rem] md:max-w-2xl md:p-20 lg:max-w-[1500px] lg:m-0 xl:pt-0 h-screen overflow-hidden"
     >
-      <h1 className="text-[14px] lg:text-xl  uppercase tracking-[8px] ml-1 mb-3 font-medium">
+      <h1 className="text-[14px] lg:text-xl   ml-1 mb-3 font-medium">
         Hi, the name is
       </h1>
-      <h1 className="font-bold opacity-95 text-3xl md:text-7xl lg:text-6xl pb-1 md:pb-5 tracking-wider">
+      <h1 className="font-programme font-bold opacity-95 text-3xl md:text-7xl lg:text-6xl pb-1 md:pb-5 ">
         Ayodeji Atanda.
       </h1>
-      <div className="font-bold mt-1 text-3xl md:text-7xl text-gray-500 md:pb-7 tracking-wider lg:text-6xl ">
+      <div className="font-bold mt-1 text-3xl md:text-7xl text-gray-500 md:pb-7 lg:text-6xl ">
         <span>{text}</span>
         <Cursor cursorColor="#0077b5" />
       </div>
 
-      <p className="text-xl text-left mt-5 lg:mt-0 md:max-w-[43rem] lg:mb-10 font-medium md:text-xl tracking-wider ml-1">
-        I’m a software engineer specializing in building (and occasionally
-        designing) exceptional digital experiences. Currently, I’m focused on
-        building accessible, human-centered products at Upstatement. Currently,
-        I’m focused on building accessible, human-centered products at
-        Upstatement.
+      <p className="text-xl text-left mt-5 lg:mt-0 md:max-w-[43rem] lg:mb-10  xl:mb-5 font-medium md:text-xl  lg:text-2xl xl:text-xl ml-1">
+      I'm a freelance software engineer with a passion for crafting exceptional digital experiences. 
+      My expertise lies in building accessible and human-centered products. As a freelancer, I have the flexibility
+       to work on various projects and I'm currently available for new opportunities. 
+      Let's collaborate to create outstanding solutions tailored to your needs.
       </p>
       <Button title="Get In Touch" />
     </motion.div>
