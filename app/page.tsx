0c3d3814b5eaea26a1d0b4c9f150559e58fc4848
Ayodeji Atanda <41900188+deji-ice@users.noreply.github.com>
2023-06-10 01:13:21 +0100

    Revert "re edit for small screens"

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import "./globals.css";
 import bg from "../Assets/stacked-peaks-haikei (3).png";
 import Hero from "@/components/Hero";
@@ -7,12 +6,10 @@ import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
-import SmoothScroll from '@/components/SmoothScroll';
 
 
 export default function Home() {
   return (
-    
     <div
       id="page"
       className="font-programme antialiased text-black snap-mandatory snap-y z-0 m-3 text-4xl
@@ -20,7 +17,6 @@ export default function Home() {
      bg-[white] bg-center bg-fixed"
       style={{ backgroundImage: `url(${bg})` }}
     >
-      
       <section id="hero" className="snap-start">
         <Hero />
       </section>
