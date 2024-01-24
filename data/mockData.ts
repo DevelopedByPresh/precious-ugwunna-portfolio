fafeb2d092c3588eab4a86a7b2d3d59487f32ccb
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-01-24 14:24:57 +0100

    added data

--- data/mockData.ts
+++ data/mockData.ts
@@ -43,33 +43,24 @@ type Project = {
 export const projectss: Project[] = [
     {
         id: 1,
-        projectName: "Photographer Portfolio Showcase",
-        projectImage: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1691492785/cartizn/Screenshot_2023-08-08_100540_txylyj.png",
-        projectDescription: "An Artistic Fusion of Technology and Imagery, i crafted a captivating portfolio website for a photographer using React, Framer Motion, and GSAP to bring imagery to life. Leveraged Tailwind CSS for a polished design and integrated Cloudinary for seamless image loading. Collaborated with a designer to harmonize artistry and technology, resulting in a visually stunning digital showcase.",
-        demoLink: "https://cartizn.vercel.app/",
-        githubLink: "https://github.com/deji-ice/cartizn",
-        tools: ["GSAP", "Framer Motion", "React", "React-Router", "Javascript", "TailwindCSS", "SEO", "EmailJs", "Cloudinary", "Vercel"]
+        projectName: "Custom Multiselect Component",
+        projectImage: multiselect,
+        projectDescription: "A React and TypeScript-based multiselect component created from scratch with CSS modules. Allows users to select multiple options from a dropdown list. Highly customizable, performant, and integrates seamlessly with React applications. Demonstrates expertise in building reusable UI components with a focus on flexibility and maintainability",
+        demoLink: "https://multi-select-typescipt.netlify.app/",
+        githubLink: "https://github.com/deji-ice/multi-select-typescript",
+        tools: ["Typescript", "React", "CSS", "Netlify"]
     },
     {
         id: 2,
-        projectName: "Software Developer Portfolio Showcase",
-        projectImage: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1691494937/cartizn/Screenshot_2023-08-08_124122_hssdkd.png",
-        projectDescription: "Crafted an SEO-optimized Developer Portfolio using Framer Motion, Next.js, TypeScript, and Tailwind CSS. Seamlessly integrated Spotify API and Cloudinary for enhanced user experience and image optimization. Explore my journey, tech expertise, and standout projects through this interactive showcase designed to leave a lasting digital impression.",
-        demoLink: "https://ayodejiatanda.vercel.app/",
-        githubLink: "https://github.com/deji-ice/Mainportfoli0",
-        tools: ["Framer Motion", "NextJS", "Typescript", "TailwindCSS", "Spoitfy API", "SEO", "Cloudinary", "Vercel"]
+        projectName: "ChatGPT Clone",
+        projectImage: chatgpt,
+        projectDescription: "A web application built with TypeScript, Next.js 13, and Tailwind CSS that allows users to chat with an AI language model. Implemented Next.js server-side rendering and utilized Firebase for real-time communication. Implemented NextAuth for user authentication. Developed form validation with Yup. This project showcases my skills in building modern, scalable web applications using the latest technologies.",
+        demoLink: "https://chat-gpt-vlone.vercel.app/",
+        githubLink: "https://github.com/deji-ice/chat-gpt",
+        tools: ["Typescript", "NextJS", "Tailwind CSS", "Next Auth", "Open AI"]
     },
     {
         id: 3,
-        projectName: "Blog with Embedded Sanity CMS",
-        projectImage: blog,
-        projectDescription: "A personal blog website using Next.js 13 and TypeScript, with an embedded content management system from Sanity. Leveraged Tailwind CSS for responsive design and utilized Next.js SSG and revalidation for optimal performance. Implemented dynamic routing to generate pages for each blog post and used server-side rendering to enhance SEO. Ensured a seamless user experience with lazy loading and image optimization.",
-        demoLink: "https://thecodechronicles.vercel.app/",
-        githubLink: "https://github.com/deji-ice/nextjs-blog",
-        tools: ["Sanity", "Typescript", "NextJS", "TailwindCSS", "Heroicons", "Vercel"]
-    },
-    {
-        id: 4,
         projectName: "Africash Fintech Landing Page",
         projectImage: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1691494434/cartizn/Screenshot_2023-08-08_123244_tklbio.png",
         projectDescription: "Africash is a fintech landing page built with React, Tailwind CSS, and Framer Motion. The landing page is designed to promote the Africash financial platform with a modern and responsive user interface. Features include easy navigation, smooth animations, and informative sections highlighting the platform's key benefits.",
@@ -78,24 +69,49 @@ export const projectss: Project[] = [
         tools: ["TailwindCSS", "React", "Framer Motion", "Netlify"]
     },
     {
-        id: 5,
-        projectName: "ChatGPT Clone",
-        projectImage: chatgpt,
-        projectDescription: "A web application built with TypeScript, Next.js 13, and Tailwind CSS that allows users to chat with an AI language model. Implemented Next.js server-side rendering and utilized Firebase for real-time communication. Implemented NextAuth for user authentication. Developed form validation with Yup. This project showcases my skills in building modern, scalable web applications using the latest technologies.",
-        demoLink: "https://chat-gpt-vlone.vercel.app/",
-        githubLink: "https://github.com/deji-ice/chat-gpt",
-        tools: ["Typescript", "NextJS", "Tailwind CSS", "Next Auth", "Open AI"]
+        id: 4,
+        projectName: "Blog with Embedded Sanity CMS",
+        projectImage: blog,
+        projectDescription: "A personal blog website using Next.js 13 and TypeScript, with an embedded content management system from Sanity. Leveraged Tailwind CSS for responsive design and utilized Next.js SSG and revalidation for optimal performance. Implemented dynamic routing to generate pages for each blog post and used server-side rendering to enhance SEO. Ensured a seamless user experience with lazy loading and image optimization.",
+        demoLink: "https://thecodechronicles.vercel.app/",
+        githubLink: "https://github.com/deji-ice/nextjs-blog",
+        tools: ["Sanity", "Typescript", "NextJS", "TailwindCSS", "Heroicons", "Vercel"]
     },
-
-
     {
+        id: 5,
+        projectName: "Software Developer Portfolio Showcase",
+        projectImage: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1691494937/cartizn/Screenshot_2023-08-08_124122_hssdkd.png",
+        projectDescription: "Crafted an SEO-optimized Developer Portfolio using Framer Motion, Next.js, TypeScript, and Tailwind CSS. Seamlessly integrated Spotify API and Cloudinary for enhanced user experience and image optimization. Explore my journey, tech expertise, and standout projects through this interactive showcase designed to leave a lasting digital impression.",
+        demoLink: "https://ayodejiatanda.vercel.app/",
+        githubLink: "https://github.com/deji-ice/Mainportfoli0",
+        tools: ["Framer Motion", "NextJS", "Typescript", "TailwindCSS", "Spoitfy API", "SEO", "Cloudinary", "Vercel"]
+    },
+        {
         id: 6,
-        projectName: "Custom Multiselect Component",
-        projectImage: multiselect,
-        projectDescription: "A React and TypeScript-based multiselect component created from scratch with CSS modules. Allows users to select multiple options from a dropdown list. Highly customizable, performant, and integrates seamlessly with React applications. Demonstrates expertise in building reusable UI components with a focus on flexibility and maintainability",
-        demoLink: "https://multi-select-typescipt.netlify.app/",
-        githubLink: "https://github.com/deji-ice/multi-select-typescript",
-        tools: ["Typescript", "React", "CSS", "Netlify"]
+        projectName: "AirBnb Clone",
+        projectImage: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1706101494/Screenshot_63_mkyrpo.png",
+        projectDescription: "Crafting an Airbnb-inspired frontend clone with TypeScript and static data. Elevate the user experience with seamless property browsing, responsive design, and an intuitive interface. A showcase of design and functionality inspired by Airbnb's renowned simplicity.",
+        demoLink: "https://airbnb-500-c.vercel.app/",
+        githubLink: "https://github.com/deji-ice/airbnb-500C/",
+        tools: [ "React", "TypeScript", "TailwindCSS", "DaisyUI", "Unsplash" ,"Cloudinary", "Vercel"]
+    },
+    {
+        id: 7,
+        projectName: "Photographer Portfolio Showcase",
+        projectImage: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1691492785/cartizn/Screenshot_2023-08-08_100540_txylyj.png",
+        projectDescription: "An Artistic Fusion of Technology and Imagery, i crafted a captivating portfolio website for a photographer using React, Framer Motion, and GSAP to bring imagery to life. Leveraged Tailwind CSS for a polished design and integrated Cloudinary for seamless image loading. Collaborated with a designer to harmonize artistry and technology, resulting in a visually stunning digital showcase.",
+        demoLink: "https://cartizn.vercel.app/",
+        githubLink: "https://github.com/deji-ice/cartizn",
+        tools: ["GSAP", "Framer Motion", "React", "React-Router", "Javascript", "TailwindCSS", "SEO", "EmailJs", "Cloudinary", "Vercel"]
+    },
+    {
+        id: 8,
+        projectName: "HarvestGrove",
+        projectImage: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1706018424/mines/Screenshot_62_xmbyau.png",
+        projectDescription:"Transforming a Figma design into a vibrant HarvestGrove landing page, I utilized React, Framer Motion, and GSAP for dynamic animations. With Tailwind CSS and Cloudinary, the result is a visually stunning and responsive digital showcase that harmonizes artistry and technology.",
+        demoLink: "https://harvestgrove.vercel.app",
+        githubLink: "https://github.com/deji-ice/HarvestGrovee",
+        tools: ["GSAP", "Framer Motion", "React", "Javascript", "TailwindCSS", "SEO", "Cloudinary", "Vercel"]
     },
 ];
 
