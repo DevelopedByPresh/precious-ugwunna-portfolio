19f06c4148db3318ea147c982f670ab6025fa595
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-12-12 10:34:38 +0100

    Update Hero component to include new role as Smart Contract Developer and enhance personal description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,12 @@ type Props = {};
 
 const Hero = ({}: Props) => {
   const [text, count] = useTypewriter({
-    words: ["Fullstack Web Developer.", "Frontend Developer.", "Graphic Designer."],
+    words: [
+      "Fullstack Web Developer.",
+      "Frontend Developer.",
+      "Smart Contract Developer.",
+      "Graphic Designer.",
+    ],
     loop: true,
     delaySpeed: 2000,
   });
@@ -18,23 +23,27 @@ const Hero = ({}: Props) => {
       className="flex flex-col m-5 xl:ml-[10rem] lg:pb-[18rem] items-start mt-[8rem] md:mt-[15rem]
     lg:mt-[20rem] xl:mt-[8rem] md:max-w-2xl md:p-20 lg:max-w-[1500px] lg:m-0 xl:pt-0 h-screen overflow-hidden"
     >
-      <h4 className="text-[14px]  lg:text-xl   ml-1 mb-3 font-medium">
+      <h4 className="text-[14px] lg:text-xl ml-1 mb-3 font-medium">
         Hi, the name is
       </h4>
-      <h1 className="font-programme font-bold opacity-95 text-4xl md:text-7xl lg:text-6xl pb-1 md:pb-5 ">
+      <h1 className="font-programme font-bold opacity-95 text-4xl md:text-7xl lg:text-6xl pb-1 md:pb-5">
         Ayodeji Atanda.
       </h1>
-      <div className="font-bold mt-1 text-4xl md:text-7xl text-gray-500 md:pb-7 lg:text-6xl ">
-        <h2>{text}
-         <Cursor cursorColor="#0077b5" /></h2>
-       
+      <div className="font-bold mt-1 text-4xl md:text-7xl text-gray-500 md:pb-7 lg:text-6xl">
+        <h2>
+          {text}
+          <Cursor cursorColor="#0077b5" />
+        </h2>
       </div>
 
-      <p className="text-lg text-left mt-5 lg:mt-0 md:max-w-[43rem] lg:mb-10  xl:mb-5 font-medium md:text-xl  lg:text-2xl xl:text-xl ml-1">
-      I'm a freelance Fullstack Web Developer with a passion for crafting exceptional digital experiences.
-       My expertise lies in building accessible, human-centered, and SEO-friendly products. As a freelancer, 
-       I have the flexibility to work on various projects and I'm currently available for new opportunities.
-        Let's collaborate to create outstanding solutions tailored to your needs.
+      <p className="text-lg text-left mt-5 lg:mt-0 md:max-w-[43rem] lg:mb-10 xl:mb-5 font-medium md:text-xl lg:text-2xl xl:text-xl ml-1">
+        I am a versatile developer skilled in both Web2 and Web3 technologies,
+        focusing on creating innovative and accessible digital experiences. My
+        expertise spans developing intuitive full-stack applications, smart
+        contracts, and decentralized applications (dApps). I thrive on bringing
+        seamless, high-quality solutions to life, combining creativity with
+        technical precision, and am deeply passionate about transforming designs
+        into reality.
       </p>
       <Button title="Get In Touch" />
     </motion.div>
