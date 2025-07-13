43355d8d1790a98047787b52e540b0476629ba1a
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-07-13 21:29:50 +0100

    Update layout

--- components/WorkSection.tsx
+++ components/WorkSection.tsx
@@ -3,8 +3,6 @@ import RecentProjects from "./RecentProjects";
 import WorkExperience from "./WorkExperience";
 
 const WorkSection = () => {
-  // Get top 3 latest projects
-
   return (
     <div className=" w-full flex flex-col lg:flex-row lg:justify-between gap-16 lg:gap-28  mt-36 max-w-7xl mx-auto px-3 md:px-16  lg:px-20 py-16">
 
